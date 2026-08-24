e18be546b9dd6e5b0d9d6b62eac4fd8426b2989f
yoantran <s3927023@rmit.edu.vn>
2026-08-24 20:58:06 +0700

    added main.py for part2

---
+++ src/part2/main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys, random
+from pygame.math import Vector2
+
+def main():
+    dt: float = clock.tick(FPS) / 1000.0
+    running = True
+    while running:
+        pass
+    
+    pygame.quit()
+    sys.exit(0)
+    
+if __name__ == "__main__":
+    main()
