87044c55f135d7d64e85bc3d960c2aacb63fc959
yoantran <s3927023@rmit.edu.vn>
2026-08-24 21:51:13 +0700

    updated main.py and config.py

---
+++ src/part2/game/config.py
@@ -0,0 +1,12 @@
+# window size
+WIDTH, HEIGHT = 800, 600
+
+# frame per second
+FPS = 60
+
+# game setup
+PLAYER_HEALTH = 20
+ENEMY_HEALTH = 5
+
+# colors
+BG = (39, 35, 47)
--- src/part2/main.py
+++ src/part2/main.py
@@ -1,13 +1,23 @@
 import pygame
 import sys, random
 from pygame.math import Vector2
+from game.config import *
 
 def main():
-    dt: float = clock.tick(FPS) / 1000.0
+    pygame.init()
+    pygame.display.set_caption("Space Defense")
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("consolas", 22)
+       
     running = True
     while running:
-        pass
-    
+        dt: float = clock.tick(FPS) / 1000.0
+        screen.fill(BG)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
     pygame.quit()
     sys.exit(0)
     
