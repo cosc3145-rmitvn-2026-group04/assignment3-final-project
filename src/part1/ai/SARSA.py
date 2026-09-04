85153d21495f1c09de2b0bb3e8db8d99d3f7280a
yoantran <s3927023@rmit.edu.vn>
2026-09-04 21:50:43 +0700

    SARSA level 1 completed

--- src/part1/ai/SARSA.py
+++ src/part1/ai/SARSA.py
@@ -1,13 +1,21 @@
 from .RLAgent import RLAgent
 
 class SARSAAgent(RLAgent):
-    def update(self, state, action, reward, next_state, next_action, done):
+    def update(
+        self, 
+        state: tuple, 
+        action: int, 
+        reward: float, 
+        next_state: tuple, 
+        next_action: int, 
+        done: bool
+        ) -> None:
         values = self.get_values(state)
+        
         old_value = values[action]
 
-        if done:
-            target = reward
-        else:
-            target = reward + self.gamma * self.get_values(next_state)[next_action]
+        target = reward
+        if not done:
+            target += self.gamma * self.get_values(next_state)[next_action]
 
-        values[action] = old_value + self.alpha * (target - old_value)
+        values[action] += self.alpha * (target - old_value)
