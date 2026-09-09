06a1a4ef4ef00b161a1906fed322ee8048940371
miketvo <miketvo@outlook.com>
2026-09-09 20:30:38 +0700

    Part 1: Reformatted code in part1.ai.q_learning and SARSA

--- src/part1/ai/q_learning.py
+++ src/part1/ai/q_learning.py
@@ -2,23 +2,19 @@ from .RLAgent import RLAgent
 
 class QLearningAgent(RLAgent):
     def update(
-        self, 
+        self,
         state: tuple,
         action: int,
         reward: float,
         next_state: tuple,
         done: bool
-        ) -> None:
+    ) -> None:
         values = self.get_values(state)
         old_value = values[action]
 
         if done:
             target = reward
         else:
-            target = reward + self.gamma * max(
-                self.get_values(next_state)
-                )
+            target = reward + self.gamma * max(self.get_values(next_state))
 
-        values[action] = old_value + self.alpha * (
-            target - old_value
-            )
+        values[action] = old_value + self.alpha * (target - old_value)
